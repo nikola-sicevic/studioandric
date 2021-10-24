76576336b2c957b4c80725729723914084ba2028
Nikola Sicevic <nikolasicevic@live.com>
2021-10-24 17:17:01 +0200

    Show/hide for mobile menu refactored

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $( document ).ready(function() {
     // Open navigation for mobile phones
     $('#btn-menu-open').click(function() {
-        $('#menu-sm').show();
+        $('#menu-sm').css({
+            "display": "flex"
+        });
     });
 
     // Close menu for mobile phones
     $('#btn-menu-close').click(function() {
-        $('#menu-sm').hide();
+        $('#menu-sm').css({
+            "display": "none"
+        });
     });
 });
