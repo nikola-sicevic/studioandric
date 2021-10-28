7c5a83380f29bf7fdfaacaaeab5211cc8d78f211
Nikola Sicevic <nikolasicevic@live.com>
2021-10-28 22:41:42 +0200

    Home page static layout finished

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 $( document ).ready(function() {
     // Open navigation for mobile phones
-    $('#btn-menu-open').click(function() {
+    $('#btn-menu-open').click(function(e) {
+        e.preventDefault();
+
         $('#menu-sm').css({
             "display": "flex"
         });
     });
 
     // Close menu for mobile phones
-    $('#btn-menu-close').click(function() {
+    $('#btn-menu-close').click(function(e) {
+        e.preventDefault();
+
         $('#menu-sm').css({
             "display": "none"
         });
