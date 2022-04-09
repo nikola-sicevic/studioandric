3c72d285d997f843aed030d2ddf0fe8c73cb4de1
Nikola Sicevic <nikolasicevic@live.com>
2022-04-09 20:11:52 +0200

    Final changes (again)

--- images.js
+++ images.js
@@ -2,69 +2,69 @@ let images = [
     {
         "alt": "Listovi nepokretnosti",
         "mobile": {
-            "pageName": "listovi_nepokretnosti_page_sm.png",
-            "landingName": "listovi_nepokretnosti_sm.png",
+            "pageName": "listovi_nepokretnosti_page_lg.png",
+            "landingName": "listovi_nepokretnosti_lg.png",
             "height": 469,
-            "padding": 50,
-            "totalBottom": 519
+            "padding": 100,
+            "totalBottom": 569
         },
         "desktop": {
             "pageName": "listovi_nepokretnosti_page_lg.png",
             "landingName": "listovi_nepokretnosti_lg.png",
-            "height": 1554,
-            "padding": 50,
-            "totalBottom": 1604
+            "height": 1738,
+            "padding": 100,
+            "totalBottom": 1838
         },
     },
     {
         "alt": "Ribarnica",
         "mobile": {
-            "pageName": "ribarnica_page_sm.png",
-            "landingName": "ribarnica_sm.png",
-            "height": 349,
-            "padding": 50,
-            "totalBottom": 918
+            "pageName": "ribarnica_page_lg.png",
+            "landingName": "ribarnica_lg.png",
+            "height": 1265,
+            "padding": 100,
+            "totalBottom": 2400
         },
         "desktop": {
             "pageName": "ribarnica_page_lg.png",
             "landingName": "ribarnica_lg.png",
             "height": 1126,
-            "padding": 50,
-            "totalBottom": 2780
+            "padding": 100,
+            "totalBottom": 3064
         },
     },
     {
         "alt": "Tijana Vukan",
         "mobile": {
-            "pageName": "tijana_vukan_page_sm.png",
-            "landingName": "tijana_vukan_sm.png",
-            "height": 682,
+            "pageName": "tijana_vukan_page_lg.png",
+            "landingName": "tijana_vukan_lg.png",
+            "height": 1357,
             "padding": 0,
-            "totalBottom": 1650
+            "totalBottom": 3757
         },
         "desktop": {
             "pageName": "tijana_vukan_page_lg.png",
             "landingName": "tijana_vukan_lg.png",
-            "height": 2181,
+            "height": 2467,
             "padding": 0,
-            "totalBottom": 5011
+            "totalBottom": 5531
         },
     },
     {
         "alt": "VI Athlete",
         "mobile": {
-            "pageName": "vi_athlete_page_sm.png",
-            "landingName": "vi_athlete_sm.png",
-            "height": 361,
+            "pageName": "vi_athlete_page_lg.png",
+            "landingName": "vi_athlete_lg.png",
+            "height": 717,
             "padding": 0,
-            "totalBottom": 2011
+            "totalBottom": 5728
         },
         "desktop": {
             "pageName": "vi_athlete_page_lg.png",
             "landingName": "vi_athlete_lg.png",
-            "height": 1152,
+            "height": 1303,
             "padding": 0,
-            "totalBottom": 6163
+            "totalBottom": 6834
         },
     },
 ];
